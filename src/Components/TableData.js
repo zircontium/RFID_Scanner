c3d691a965ec755e19b36ee680846fddb7f38b97
Sabuj Golui <sabujgolui5#gmail.com>
2023-03-03 13:31:58 +0530

    added RFID Web UI

---
+++ src/Components/TableData.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Table() {
+  return (
+    <div className="mt-4">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Serial No.:</th>
+            <th scope="col">Tag ID</th>
+            <th scope="col">VRN</th>
+            <th scope="col">Time</th>
+            <th scope="col">State</th>
+            <th scope="col">Remarks</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>@fat</td>
+            <td>@fat</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
