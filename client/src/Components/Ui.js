02f43f36806af6b94cd6cfdc00b8ac54d0808b16
Sabuj Golui <sabujgolui5#gmail.com>
2023-03-17 13:36:15 +0530

    fixed package error

--- client/src/Components/Ui.js
+++ client/src/Components/Ui.js
@@ -2,20 +2,19 @@ import React from "react";
 import "./Ui.css";
 import Table from "./TableData";
 
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-
 function Ui() {
   return (
     <div>
       <div className="container">
-        <Form>
-          <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
-            <Form.Label>Search by RFID no.:</Form.Label>
-            <Form.Control type="email" placeholder="Enter RFID NO" />
-          </Form.Group>
-          <Button type="submit">Search</Button>
-        </Form>
+        <form>
+          <div className="mb-3 mt-4" controlId="formBasicEmail">
+            <label>Search by RFID no.:</label>
+            <input className="form-control"></input>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Search
+          </button>
+        </form>
         <Table />
       </div>
     </div>
